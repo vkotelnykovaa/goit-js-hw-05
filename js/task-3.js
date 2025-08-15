f1e64db2e60c97a4b5400b0f67efe01be60556f6
vkotelnykovaa <lerkotelnikova6@gmail.com>
2025-08-15 09:35:39 +0300

    homework is ready

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
-const sortByDescendingFriendCount = (users) => users
-   .toSorted((a, b) => a.friends.length - b.friends.length);
+const sortByDescendingFriendCount = (users) =>
+  users.toSorted((a, b) => b.friends.length - a.friends.length);
 
    console.log(
   sortByDescendingFriendCount([
